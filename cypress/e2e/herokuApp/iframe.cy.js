74448e1ba39e7e4fcc1fddfd563f4c4620fa4009
sandeep kumar c <sandeep@everest.engineering>
2023-12-01 20:10:43 +0530

    add cypress parallel

--- cypress/e2e/herokuApp/iframe.cy.js
+++ cypress/e2e/herokuApp/iframe.cy.js
@@ -5,12 +5,8 @@ describe('handle iframe',()=>{
         cy.visit('https://the-internet.herokuapp.com/iframe')
         cy.wait(2000)
         cy.viewport(2000, 1600)
-
-        cy.visit('https://google.com')
-        cy.wait(2000)
         cy.get('#mce_0_ifr').then(($iframe)=>{
             const body = $iframe.contents().find('body')
-            cy.contains('save').filter(':visible')
             cy.wrap(body).xpath('//body[@id="tinymce"]/p').as('txtarea')
             cy.wait(2000)
             cy.get('@txtarea').type('Hello world')
